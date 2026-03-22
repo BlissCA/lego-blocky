9f613fac65a3edc6ca633b24d5b7a500dc7f4010
YvesLev <yveslev@gmail.com>
2026-03-22 00:00:31 -0400

    Add Rcx Motor blocks

--- blocks/lego_blocks.js
+++ blocks/lego_blocks.js
@@ -952,7 +952,7 @@ Blockly.Blocks['Rcx_MotPort'] = {
   init: function() {
     this.appendDummyInput()
       .appendField(new Blockly.FieldDropdown([
-        ["A", "1"], ["B", "2"], ["C", "4"], , ["A+B", "3"], ["A+C", "5"], ["B+C", "6"], ["A+B+C", "7"]
+        ["A", "1"], ["B", "2"], ["C", "4"], , ["AB", "3"], ["AC", "5"], ["BC", "6"], ["ABC", "7"]
       ]), "LETTER");
 
     this.setOutput(true, "Number");
